b5bde06d974045f1ae4411886eaa22379a6ab7b1
foreigndalone <borodkin1elvis@gmail.com>
2025-11-03 15:58:55 +0200

    Added API to get all champs on the main

---
+++ claint-LOL-Chapms/src/components/RoleFilter.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useState } from 'react';
+
+const RoleFilter = ({tags, allChamps, setChampions}) => {
+const [numberOf, setNumberOf] = useState('')
+
+const handleRoleClick = (tag) => {
+    const filtered = allChamps.filter(champ =>
+        champ.tags.includes(tag)
+        );
+    setNumberOf(`Number of ${tag}s : ${filtered.length}`)
+    setChampions(filtered);
+}
+  return (
+    <div>
+
+    {numberOf?<p>{numberOf}</p>:<></>}
+        {tags.map((tag)=>(
+            <button key={tag} onClick={()=>handleRoleClick(tag)}>{tag}</button>
+        ))}
+    </div>
+  )
+}
+
+export default RoleFilter
---
+++ claint-LOL-Chapms/src/components/Search.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+const Search = ({ allChamps, setChampions }) => {
+  const [value, setValue] = useState("");
+
+  const handleSearch = (e) => {
+    const query = e.target.value.toLowerCase();
+    setValue(e.target.value);
+
+    if (!query.trim()) {
+      setChampions(allChamps);
+      return;
+    }
+
+    const filtered = allChamps.filter(champ =>
+      champ.name.toLowerCase().includes(query)
+    );
+
+    setChampions(filtered);
+  };
+
+  return (
+    <div className="search-bar">
+      <input type="text" value={value} onChange={handleSearch} placeholder="Search by name" className="search-input"/>
+    </div>
+  );
+};
+export default Search
